dc71961bce7cb6c099cfc038638a354322d1e0dd
marcus <846079865@qq.com>
2017-07-25 08:08:10 +0800

    完善

---
+++ src/model/app_stock_in.js
@@ -0,0 +1,27 @@
+import constant from '../util/constant';
+
+export default {
+
+    namespace: 'app_stock_in',
+
+    state: {
+        app_id: '',
+        app_list: [],
+        total: 0,
+        page_index: 1,
+        page_size: constant.page_size,
+        list: [],
+        stock_type: 'APP',
+        product_name: '',
+        warehouse_id: '',
+        warehouse_list: [],
+        product_list: []
+    },
+
+    reducers: {
+        fetch(state, action) {
+            return {...state, ...action.data};
+        }
+    }
+
+};
